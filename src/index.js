96b5ef382ce13f9cdd5ac36799d4e1cb22409536
Prakash Mahat <shadyprakash8@gmail.com>
2021-08-15 22:10:24 +0545

    babel setup

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ const elvenShieldRecipe = {
 	ironIngot: 1,
 	refinedMoonstone: 4,
 };
-
+const elvenGauntletsRecipe = {
+	...elvenShieldRecipe,
+	leather: 1,
+	refinedMoonstone: 4,
+};
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
